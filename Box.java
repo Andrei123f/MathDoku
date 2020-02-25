a0580fb4428efb8310912804259064b186d4d512
Andrei <andrei.popabd@gmail.com>
2020-02-25 23:29:18 +0000

    FINISHes the drawing for 'search for neighbours' algorithm

---
+++ Box.java
@@ -0,0 +1,39 @@
+package gui;
+
+import java.util.ArrayList;
+
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Text;
+
+public class Box {
+	private int boxId;
+	private Text text;
+	private Rectangle border;
+	private ArrayList<Box> set;
+	
+	public Box() {
+		border=new Rectangle(100,100);
+		border.setFill(null);
+		border.setStroke(Color.BLACK);
+		border.setStrokeWidth(1);
+	}
+	
+	public void setBoxId(int id) {
+		this.boxId=id;
+	}
+	public void setSet(ArrayList<Box> set) {
+		this.set=set;
+	}
+	public ArrayList<Box> getSet(){
+		return this.set;
+	}
+	public int getBoxId() {
+	return this.boxId;	
+	}
+	
+	public Rectangle getBorder() {
+		return this.border;
+	}
+	
+}
--- Coursework.java
+++ Coursework.java
@@ -1,69 +1,148 @@
 package gui;
-/*
- * go trhough each node and check its neighbours
- * if they are part of the same cage, do not draw a line between them
- * if they are not, draw a line between them
- */
+
 import java.util.ArrayList;
 
 import javafx.application.Application;
-import javafx.geometry.Pos;
-import javafx.scene.Parent;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
-import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.Line;
 import javafx.scene.shape.Rectangle;
-import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
-public class Coursework extends Application {
-	//the main class
-	public static void main(String[] args) {
-		launch(args);
+public class Coursework extends Application{
+	private Pane root=new Pane();
+	private int numberOfCages=15;
+	private ArrayList<ArrayList> cages=new ArrayList<ArrayList>();
+	private ArrayList<Box> boxes=new ArrayList<Box>();
+	
+	public void prepareObjects() {
+		for(int i=1;i<=numberOfCages;i++)
+			cages.add(new ArrayList<Box>());
+		
+	}
+	public void searchNeighnours() {
+			System.out.println();
+			int column=1,i=0,j;
+			for(Box box:boxes) {
+				int boxId=box.getBoxId();
+				j=i; //the last element 
+				i++;	//the current element
+				
+				if(i!=36) {
+					int currBoxId=boxes.get(i).getBoxId();
+					int lastBoxId=boxes.get(j).getBoxId();
+					if(currBoxId < lastBoxId) {
+						column++;
+						System.out.println("New column! : " + column);
+					}
+				}
+				/*
+				if(column==1) {
+					System.out.println("The box with the id of " + boxId +" has no left neighbour");
+				}
+				else if(column == 6) {
+					System.out.println("The box with the id of "+ boxId+" has no right neighbour");
+				}
+				else {
+					System.out.println("The box with the id of "+boxId+" has left and right neighbours");
+				}
+				if(boxId-6>=1) {
+					System.out.println("The box with the id of " + boxId +" has an upper neighbour");
+				}
+				else {
+					System.out.println("The box with the id of " + boxId +" does not have an upper neighbour");
+				}
+				*/
+				 if(boxId+6<=36) {
+					 System.out.println("The box with the id of " + boxId +" has an below neighbout");
+					 Rectangle border=box.getBorder();
+					double x=border.getX();
+					double y=border.getY();
+					double width=border.getWidth();
+					double height=border.getHeight();
+					 
+					 System.out.println(boxId+" X:"+x);
+					 System.out.println(boxId+" Y:"+y);
+					 
+					 double startX=x;
+					 double startY=y+height;
+					 double endX=x+width;
+					 double endY=startY;
+					 Line line=new Line(startX,startY,endX,endY);
+					 line.setStroke(Color.RED);
+					 line.setStrokeWidth(2);
+					 root.getChildren().add(line);
+				 }
+				 else {
+					 //System.out.println("The box with the id of " + boxId +" does not have below neighbour");
+				 }
+				
+				
+			}
+			
 	}
 	
-	//the class that sets everything up
-	private class Setter{
-		private int numberOfCages=15;
-		private ArrayList<ArrayList> cages;
-		private ArrayList<Box> boxes;
-		
-		public Setter() {
-			boxes=new ArrayList<Box>();
-			cages=new ArrayList<ArrayList>();
-			for(int i=1;i<=numberOfCages;i++) {
-				cages.add(new ArrayList<Box>());
-			}		
+	public void drawCage(Box box,boolean r,boolean l, boolean u,boolean d) {
+		ArrayList<Box> cage=box.getSet();
+		int boxId=box.getBoxId();
+		if(r) { //right			
+			
+			
 		}
-		
-	//the function that creates the view
-	private Parent createContent(){
-		Pane root=new Pane();
-		root.setPrefSize(800, 800);
+		if(l) {//left
+			
+		}
+		if(u) {//up
+			
+		}
+		if(d) {//down
+			
+		}
+	}
+	
+
+	public void start(Stage stage) throws Exception {
+		stage.setTitle("Mathdoku");
+		GridPane gridPane=new GridPane();
+		HBox hBox=new HBox(10); //parent for the buttons
+		hBox.setPadding(new Insets(10,10,10,10));
+		Button undo=new Button("undo");
+		Button redo=new Button("redo");
+		Button clear=new Button("clear");
+		TextField file=new TextField();
+		TextField input=new TextField();
+		this.prepareObjects();
 		
 		int id;
-		for(int i=1;i<=6;i++) { // horizontal
+		for(int i=1;i<=6;i++) {
 			id=i;
-			for(int j=1;j<=6;j++) { //vertical
-				Box box= new Box(id);
+			for(int j=1;j<=6;j++) {
+				Box box=new Box();
 				box.setBoxId(id);
 				boxes.add(box);
-				box.setTranslateY(j*100);
-				box.setTranslateX(i*100);
-				root.getChildren().add(box);
+				Rectangle border=box.getBorder();
+				root.getChildren().addAll(border);
+				border.setX(i*100);
+				border.setY(j*100);
 				id+=6;
 			}
 		}
-		System.out.println(boxes.get(1).getBoxId());
-		//adding some random boxes in the same cage.
-		
 		addBoxToCage(boxes.get(0),cages.get(0));
 		addBoxToCage(boxes.get(1),cages.get(0));
 		addBoxToCage(boxes.get(12),cages.get(1));
 		addBoxToCage(boxes.get(13),cages.get(1));
-		
 		ArrayList<Box> cage=cages.get(0);
+		
+		boxes.get(0).setSet(cages.get(0));
+		boxes.get(1).setSet(cages.get(0));
+		boxes.get(12).setSet(cages.get(1));
+		boxes.get(13).setSet(cages.get(1));
 		for(Box box : cage) {
 			System.out.println("The box id of the first cage: " + box.getBoxId());
 		}
@@ -72,133 +151,24 @@ public class Coursework extends Application {
 		for(Box box : cage) {
 			System.out.println("The box id of the secong cage: " + box.getBoxId());
 		}
-		drawCages();
+		searchNeighnours();
+
+		hBox.getChildren().addAll(undo,redo,clear,file);
+		root.getChildren().addAll(gridPane,hBox);
+		stage.setScene(new Scene(root));
+		stage.show();
 		
-		return root;
 	}
-	//the function that adds the box to the specific cage
-	private void addBoxToCage(Box box,ArrayList<Box> cage) {
+	
+	public void addBoxToCage(Box box, ArrayList<Box> cage) {
 		cage.add(box);
 	}
-	
-	private boolean cageContainsBox(ArrayList<Box> cage, Box box) {
-		return(cage.contains(box));
-	}
-	//for drawing the cages i will determine whether the neighbours of the box are in the same cage
-	//only if the box has neighbours!
-	//For each box, it can have a upper neighbour,bottom neighbour,left and right neighbour
-	//i need to determine on which line is situated the current box we are looking at.
-	//for line 1 the range for left - right neighbours is 1-6
-	//for line 2 the range for left - right neighbours is 7-12
-	//for line 3 the range for left - right neighbours is 13-18
-	//for line 4 the range for left - right neighbours is 19-24
-	//for line 5 the range for left - right neighbours is 25-30
-	//for line 6 the range for left - right neighbours is 31-32
-	//check if the next element boxId is less than the current element boxId
-	//if it is not, we increment row and we get the current row we are in
-	//i will use columns to determine the possible neighbours
-	//if we are on the first and sixth(n th) column of the grid :
-	//the first column does not have a left neighbour
-	//the nth column does not have a rigt neighbour
-	private void drawCages() {
-		System.out.println();
-		int column=1;
-		int i=0;
-		int j;
-		boolean nextRow;
-		for(Box box : boxes) {
-			int boxId=box.getBoxId();
-			j=i; 		//the last element of the arraylist
-			i++; 		// i the current element from the boxes arraylist
-				
-			if(i!=36) {
-				int currBoxId=boxes.get(i).getBoxId();
-				int lastBoxId=boxes.get(j).getBoxId();
-				
-				if(currBoxId < lastBoxId) {
-					column++;
-					System.out.println("New column! : " + column);
-					
-			}
-			}
-			
-			
-			if(column==1) {
-				System.out.println("The box with the id of " + boxId +" has no left neighbour");
-			}
-			else if(column == 6) {
-				System.out.println("The box with the id of "+ boxId+" has no right neighbour");
-			}
-			else {
-				System.out.println("The box with the id of "+boxId+" has left and right neighbours");
-			}
-			if(boxId-6>=1) {
-				System.out.println("The box with the id of " + boxId +" has an upper neighbour");
-			}
-			else {
-				System.out.println("The box with the id of " + boxId +" does not have an upper neighbour");
-			}
-			
-			 if(boxId+6<=36) {
-				 System.out.println("The box with the id of " + boxId +" has an below neighbout");
-			 }
-			 else {
-				 System.out.println("The box with the id of " + boxId +" does not have below neighbour");
-			 }
-			 
-			//the current element from the boxes arraylist
-			
-		}
+	public boolean isBoxInCage(Box box, ArrayList<Box> cage) {
+		return cage.contains(box);
 	}
 	
-	
+	public static void main(String[] args) {
+		launch(args);
 	}
 	
-	//The start class of the app
-	public void start(Stage stage) {
-		stage.setTitle("MathDoku");
-		Setter setter=new Setter();
-		stage.setScene(new Scene(setter.createContent()));
-		stage.show();
-	}
-	
-	
-	//The box class
-	private class Box extends StackPane{
-		private int boxId;
-		private Text text;
-		
-		
-		public Box(int indent) {
-			text=  new Text();
-			Rectangle border=new Rectangle(100,100);
-			border.setFill(null);
-			border.setStroke(Color.BLACK);
-			setAlignment(Pos.CENTER);
-			this.setText(indent);
-			
-			getChildren().addAll(border,this.text);
-		}
-		
-		
-		public void setBoxId(int id) {
-			this.boxId=id;
-		}
-		
-		
-		public int getBoxId() {
-			return (this.boxId);
-		}
-		
-		public void setText(int input) {
-			String inputS=Integer.toString(input);
-			this.text.setText(inputS);
-		}
-		
-		
-		public Text getText() {
-			return this.text;
-		}
-
-	}
 }
